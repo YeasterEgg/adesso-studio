98731a382a58e66d96805b612616e5c42b5a8a2a
Yeasteregg <l.d.mattiazzi@gmail.com>
2018-04-05 00:40:37 +0200

    first

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# REMOTE="root@46.101.114.92"
-REMOTE="pi@192.168.1.73"
-HOME_PATH="/var/www/studio"
-LOCAL_PATH="$(cd "$(dirname "$0")" && pwd)"
-
-echo "Deploying ${LOCAL_PATH} on ${REMOTE}:${REMOTE_PATH}/"
-rsync -a "${LOCAL_PATH}/" "${REMOTE}:${HOME_PATH}/." --rsync-path="sudo rsync"
-echo "Done"
