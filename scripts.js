da94fbe3cf05de6c9cf5757f6aafceb83b550490
Yeasteregg <l.d.mattiazzi@gmail.com>
2018-04-05 00:55:21 +0200

    simpler

--- scripts.js
+++ scripts.js
@@ -1,5 +1,3 @@
-const URL = '/suggestion'
-
 const generateIdxsArray = () => Array.from({ length: window.ideas.length }, (_, idx) => idx)
 let allIdxs = generateIdxsArray()
 
@@ -15,36 +13,13 @@ const suggestSomething = (ideaText, ideaLink) => () => {
   ideaText.innerHTML = newIdea.text
 }
 
-const submitForm = (form, cb) => async e => {
-  e.preventDefault()
-  const fullUrl = `${URL}?link=${form.link.value}&text=${form.text.value}`
-  const promisedFetch = new Promise((resolve, reject) => {
-    window.fetch(fullUrl).then(res => res.text()).then(resolve).catch(reject)
-  })
-  const sent = await promisedFetch
-  window.alert(sent)
-  cb()
-}
-
-const toggleForm = container => () => {
-  container.classList.toggle('flex')
-  container.classList.toggle('dn')
-}
-
 const startScript = () => {
   const another = document.getElementById('another')
   const ideaText = document.getElementById('idea-text')
   const ideaLink = document.getElementById('idea-link')
   const suggestButton = document.getElementById('suggest')
-  const formContainer = document.getElementById('form-container')
-  const formClose = document.getElementById('form-close')
-  const form = document.getElementById('form')
   const setSuggestion = suggestSomething(ideaText, ideaLink)
-  const toggleContainer = toggleForm(formContainer)
-  form.addEventListener('submit', submitForm(form, toggleContainer))
   another.addEventListener('click', setSuggestion)
-  suggestButton.addEventListener('click', toggleContainer)
-  formClose.addEventListener('click', toggleContainer)
   setSuggestion()
 }
 
